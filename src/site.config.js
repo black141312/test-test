6c6acb4afd3566f6ddd804fd28e292650dbd02de
black141312 <aditya141312@gmail.com>
2026-01-03 19:34:32 +0530

    Initial storefront generation by EpicMerch

---
+++ src/site.config.js
@@ -0,0 +1,16 @@
+export const siteConfig = {
+    name: "test -test",
+    description: "Welcome to Epic Threadz",
+    currency: "USD",
+    socials: {
+        instagram: "",
+        twitter: "",
+        facebook: "",
+    },
+    contact: {
+        email: "owner@epicthreadz.com",
+        phone: ""
+    },
+    theme: "modern" // Options: 'modern', 'playful', 'luxury', 'organic'
+};
+
